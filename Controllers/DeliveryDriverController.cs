4aaae69e3566da05447ea103f358df0b22022bbf
shrigupta <shrigupta@USGURSHRIGU78>
2021-07-22 20:56:22 +0530

    Add project files.

---
+++ Controllers/DeliveryDriverController.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Web.Mvc;
+
+
+namespace DeliveryDriverApplication.Controllers
+{
+    public class DeliveryDriverController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeliveryDriverApplication.MvcApplication" Language="C#" %>
